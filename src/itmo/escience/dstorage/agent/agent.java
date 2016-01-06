554182f27c8417e7c52a5e83a0406b54c715082a
Anton Spivak <anton.spivak@gmail.com>
2016-01-06 19:45:51 +0400

    - first commit

---
+++ src/itmo/escience/dstorage/agent/agent.java
@@ -0,0 +1,16 @@
+package itmo.escience.dstorage.agent;
+
+/**
+ *
+ * @author Anton Spivak
+ */
+public class agent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
